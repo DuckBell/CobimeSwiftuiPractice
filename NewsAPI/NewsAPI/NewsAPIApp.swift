b33945147ab25d3c03f14afa56db900154836c57
치로_이덕종 <chiro@afreecatv.com>
2022-10-25 21:18:36 +0900

    first commit

---
+++ NewsAPI/NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by DuckJong Lee on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
